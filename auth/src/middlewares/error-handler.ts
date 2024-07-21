252bceca2d34377d2857a702029d68aeaa18da05
Colson <colsoncalifornication@gmail.com>
2024-07-21 18:50:33 +0545

    add in some code to inspect the incoming error out of Request Handler and check to see if its an instance of RequestValidationError or an instance of DatabaseConnectionError and if it is, build response and send back to the user appropriately

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 export const errorHandler = (
   err: Error,
@@ -11,7 +13,15 @@ export const errorHandler = (
   // exactly how to handle all these 'n' different errors
   // @ Build one single identical structure for all possible errors across
   // all of the different services
-  console.log('Something went wrong.', err);
+  // console.log('Something went wrong.', err);
+
+  if (err instanceof RequestValidationError) {
+    console.log('handling this error as a request validation error')
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log('handling this error as a db connection error');
+  }
 
   res.status(400).send({
     // message: 'Something went wrong',
