edc72dce7cd962b4027209240a54dbfe35ce70a1
Colson <colsoncalifornication@gmail.com>
2024-07-25 12:54:13 +0545

    create database in Kubernetes && implement signup flow: take the email and password from the React app and create some entry inside of some database to reflect that user being created + add mongoose into Auth Service + make sure to create an MongoDB instance to communicate with as well :: create new instance of MongoDB inside Kubernetes Cluster: i.e run MongoDB inside a Pod -> create a deployment config file for a pod and also ClusterIP Service for inter-pods communication + connect Auth service to newly created mongoDB instance thats running inside a pod using mongoose: use the MongoDB instance running pods ClusterIP service domain name for the ConnectionString URL on mongoose to connect to MongoDB instance

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -27,6 +28,19 @@ app.all('*', async (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  // If DB connects successfully above, then its actually time for listening
+  // traffic
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!');
+  });
+};
+
+start();
