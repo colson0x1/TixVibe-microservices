ac02ebfa0fba1ea1310b04c0a50147fc9e513e62
Colson <colsoncalifornication@gmail.com>
2024-07-25 16:59:03 +0545

    make TypeScript and Mongoose to cooperate: teach TS that there are two different sets of properties -> one is properties that are passed to the constructor and second set of properties that can be accessed on the user document that gets created i.e make sure TS is going to check the type of arguments that will be passed to the User constructor just to get TS involved in the process of creating a new user by creating a new fn to create a user that has that uses TS interface and exporting for later uses

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,6 +1,14 @@
 /* @ Mongoose User Model */
 import mongoose from 'mongoose';
 
+// An interface that describes the properties that are required to create
+// a new User
+// UserAttrs -> User Attributes
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -14,4 +22,16 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
-export { User };
+// Anytime we create a new user, we're now going to call `buildUser` instead of
+// calling `new User()`
+// This should be kept in mind across all services when we try to create a new
+// user
+// This is all done to do effective type checking which cannot be done with the
+// standard `new User()` for creating a new document
+// So anytime we're going to create a new user document, we're going to call
+// this `buildUser` fn!
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+export { User, buildUser };
