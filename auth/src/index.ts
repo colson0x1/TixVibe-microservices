e0557ac66688e7971b7e69b68163c27a3a497969
Colson <colsoncalifornication@gmail.com>
2024-08-01 14:31:31 +0545

    add session support - make sure to have the ability to eventually set a cookie on a response that eventually go back to the user: install cookie-session and its type defination file + wife up to express app as a middleware  + make express aware that its behind a proxy of Ingress NGINX and to make sure it should still trust traffic as being secure even though its coming from that proxy

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,23 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+// Traffic has been proxied to our application through Ingress NGINX. Express
+// is going to see the fact that stuff is being proxied and by default. Express
+// by default doesn't really trust this HTTPS connection
+// @ Make Express aware that its behind a Proxy of Ingress NGINX and to make sure
+// it should still trust traffic as being secure even though its coming form
+// that proxy
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    // Disables encryption
+    signed: false,
+    // Require that cookies will only be used if the user is visiting the
+    // application over an HTTPS connection
+    secure: true,
+  }),
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
