35969676b8a07a1908a26857608729ea78a9ff69
Colson <colsoncalifornication@gmail.com>
2024-08-02 11:42:10 +0545

    access env variables in a pod: go into the application thats running inside the container and make sure to reference environment variable for that JWT key + if environment variable is not defined, throw an error at the instance application starts up so error is known immediately once done deployment so that something is wrong rather than capturing the error way on down the line when application is actually been running possibly for some amount of time

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -46,6 +46,12 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+  // Detect error immediately and throw an error when we start to deploy
+  // our code
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to MongoDB');
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -39,15 +39,29 @@ router.post(
     await user.save();
 
     // Generate JWT
-    const userJwt = jwt.sign({
-      id: user.id,
-      email: user.email
-    }, 'stillhome')
+    /*
+     * This doesn't throw error immediately when application starts up but 
+     * rather throws an error way on down the line when our application is 
+     * possibly been running for some amount of time
+    if (!process.env.JWT_KEY) {
+      throw new Error('Environment variable: JWT_KEY is undefined');
+    } */
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        // }, 'stillhome')
+      },
+      // process.env.JWT_KEY,
+      // This env variable is checked at the application start up time, right
+      // there in start fn of `index.ts`
+      process.env.JWT_KEY!,
+    );
 
     // Store it on session object
     req.session = {
-      jwt: userJwt
-    }
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   },
