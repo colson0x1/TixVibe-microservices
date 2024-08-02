ef60d07a5db37016587dd03345315ee9c31ecb9e
Colson <colsoncalifornication@gmail.com>
2024-08-02 14:16:06 +0545

    signin flow: do some validation on the incoming request with express-validator + see if anything went wrong during that validation attempt

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hello there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  (req: Request, res: Response) => {
+    // Gives errors on that ` req`
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // @ Requirement on RequestValidationError is that we need to pass
+      // an array of errors
+      throw new RequestValidationError(errors.array());
+    }
+  },
+);
 
 export { router as signinRouter };
