6acd6741c1d17c747bbb3b0ab81ee17163b2512e
Colson <colsoncalifornication@gmail.com>
2024-08-03 14:28:17 +0545

    implement signout: send back the header thats gonna tell the users browser to dump all the information inside of that cookie i.e remove the jwt so anytime user makes the followup request there will be no token included inside of that cookie

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,10 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  res.send('Hello there!');
+  req.session = null;
+
+  // Naturally we do still have to send back a response
+  res.send({});
 });
 
 export { router as signoutRouter };
