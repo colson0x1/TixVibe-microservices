eda5911dc9800bd59297ce7975809815c482cc05
Colson <colsoncalifornication@gmail.com>
2024-08-11 10:55:23 +0545

    add in more test around signup route handler: write test for invalid email and password and put an assertion to get back an status code of 400 on invalid credentials

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('returns a 201 on successful signup', async () => {
+  // We can either return request or await it
   return request(app)
     .post('/api/users/signup')
     .send({
@@ -9,4 +10,40 @@ it('returns a 201 on successful signup', async () => {
       password: 'stillhome',
     })
     .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'colson',
+      password: 'stillhome',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'colson',
+      password: 's',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'colson@google.com',
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'stillhome',
+    })
+    .expect(400);
 });
