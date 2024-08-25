7c7cf117a3edd171f1287324479f18370b6bde06
Colson <colsoncalifornication@gmail.com>
2024-08-25 19:12:58 +0545

    move getInitialProps + resolve issues with custom App getInitialProps - state lift the logic to fetch currentUser from landing page to app component so its centralized and removes duplication of data fetching over the components that needs to know currentUser + pass currentUser as prop from App component to header and index/landing page + add a logic to handle getInitialProps on the page trying to display and also on the custom app component i.e ensure data fetching can be done at the both location at the same time i.e resolve getInitialProps tied to individual pages not getting invoked anymore after defining getInitialProps on the custom App component by invoking getInitialProps tied to pages component from inside the getInitialProps of the custom App component i.e invoke Component.getInitialProps from custom App component to invoke LandingPage, pass down some arguments related to context so that it works correctly, let it fetch some data, take the data that comes back, and return it manually from getInitialProps of the custom App component i.e get information (api/users/currentuser) that is common for every page inside of Apps getInitialProps and also have logic to get information for a very particular page with appContext.Component.getInitialProps

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -1,5 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  // console.log(Object.keys(appContext));
+  // return {};
+  // console.log(appContext);
+
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+  // Get set of pageProps/componentProps i.e this is gonna be the set of data
+  // that we're trying to fetch from the components or the individual pages
+  // getInitialProps
+  // const pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  // Handle when getInitialProps is undefineds Example on signin/signup page
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  console.log(pageProps);
+  // console.log(data);
+
+  return data;
+};
+
+export default AppComponent;
