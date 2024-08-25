f0f1bf3e07b0c6af086f65b8098c666a27bfd87e
Colson <colsoncalifornication@gmail.com>
2024-08-25 11:38:30 +0545

    create server side rendered landing page: figure out whether or not the user is signed in at any given point in time and display on the screen if the user is signed in or not i.e make request to the /api/users/currentuser and if the user is signed in, the response from that is going to contain information like id, email of that signed in user else if not signed in, the response will be null: i.e figure out weather or not user is authenticated during server side rendering process i.e fetch data with getInitialProps static method for SSR ++ resolve mysterious connection refused error anytime trying to fetch the current user during the SSR process: configure axios a little bit differently depending on whether request is being made from the browser or NEXTjs application that is running on the container with baseURL:: rather than using client service reach out directly to auth service, instead anytime needs to fetch data from a service inside of kubernetes cluster, make the NEXTjs application reach out to Ingress NGINX which is already running inside the cluster and Ingress NGINX can figure out where it sends requests off to based upon just the path by itself + figure out someway to make sure when request is made from the NEXTjs, take a look at the original incoming request, extract the cookies off there, and include it in the request off to in Ingress NGINX so that request when routed to the Auth Service, the Auth service will see that incoming cookie :: make cross namespace communication i.e make client service thats in default namespace communicate over to ingress nginx service that is outside of default namespace with a complicated domain: http://NAMEOFSERVICE.NAMESPACE.svc.cluster.local + create an External Name Service to remap the domain of the request to ingress nginx + make sure to pass along the cookie as well ++ determine whether request is being made on the browser or the server and adjust domain for the request being issued + make ingress nginx know the domain trying to access while request is coming from the NEXT server instead of request coming from the browser on which it uses domain appended by the browser as the domain to use: resolve by specifying an options object with Host on the headers which will be used by ingress-nginx + include cookies and all the associated headers like HOST from the incoming request on the NEXT server and pass along those off to Ingress NGINX routing off them to Auth Service with cookies containg jwt

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,5 +1,56 @@
-const Auth = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser').catch((err) => console.log(err.message));
+  console.log(currentUser);
+
   return <h1 style={{ color: 'orangered' }}>TixVibe</h1>;
 };
 
-export default Auth;
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  /* const response = await axios
+    .get('/api/users/currentuser')
+    .catch((err) => console.log(err.message));
+
+  return response.data; */
+
+  if (typeof window === 'undefined') {
+    // Executed on the Server
+    // Request should be made to
+    // http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
+
+    // @ Make requests over to Ingress NGINX and route off to Auth Service
+    // Reach Ingress NGINX and specify route to forward request over to Auth Service
+    const { data } = await axios.get(
+      // 'http://SERVICENAME.NAMESPACE.svc.cluster.local'
+      // 'http://ingress-nginx.ingress.nginx.svc.cluster.local'
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        /* headers: {
+          Host: 'tixvibe.dev',
+        }, */
+        headers: req.headers,
+      },
+    );
+
+    return data;
+  } else {
+    // Executed on the Browser
+    // Request can be made with a base url of ''
+    // const response = await axios.get('/api/users/currentuser');
+    // return response.data;
+
+    const { data } = await axios.get('/api/users/currentuser');
+    /*
+     *  If user is signed out: { currentUser: null}
+     *  Or object if the user is signed in: { currentUser: { ... } }
+     */
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
