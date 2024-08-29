46f57668fbee40e4033b97df33fc638d15168301
Colson <colsoncalifornication@gmail.com>
2024-08-29 13:40:08 +0545

    test request validation for title and price on ticket creation - make sure that title and price are provided if not return some kind of error to the user: throw error if empty title or no title is provided + throw an error if wrong data for price or no price provided at all + implement logic to do the actual validation of the incoming requests: use express-validator to set error on an incoming request and inspect that request and somehow respond to it if required + wire up validateRequest middleware from common module right after the validation attempt on new-ticket.ts to make sure that all that occurs

--- tickets/src/routes/__test__/new-ticket.test.ts
+++ tickets/src/routes/__test__/new-ticket.test.ts
@@ -22,12 +22,51 @@ it('returns a status other than 401 if the user is signed in', async () => {
     .set('Cookie', global.signin())
     .send({});
 
+  // console.log(response.status);
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid title is provided', async () => {});
+it('returns an error if an invalid title is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      // Test for `title` provided but empty
+      title: '',
+      price: 25,
+    })
+    .expect(400);
 
-it('returns an error if an invalid price is provided', async () => {});
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      // Test for no `title` provided at all
+      price: 25,
+    })
+    .expect(400);
+});
+
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      // Test for `price` provided but empty
+      title: 'millionaire',
+      price: -25,
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      // Test for no `price` provided at all
+      title: 'millionaire',
+    })
+    .expect(400);
+});
 
 // Make sure everything kind of works as expected
 it('creates a ticket with valid inputs', async () => {});
