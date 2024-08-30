ef9047aa269172b9460b49a4adbb0c6e208a32b9
Colson <colsoncalifornication@gmail.com>
2024-08-30 14:04:53 +0545

    add in a check to make sure a ticket was saved to the database: @ approach - first do a check and see how many records are inside of Ticket collection, make the request and after that, once again check to see how many records are in there, and now there should be one additional record after making the request + also set a cookie while making the request for new ticket on testing :: work on the actual implementation - get title, price from the request body and save that to the db returning 201 response with ticket attached

--- tickets/src/routes/__test__/new-ticket.test.ts
+++ tickets/src/routes/__test__/new-ticket.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({});
@@ -74,11 +75,32 @@ it('returns an error if an invalid price is provided', async () => {
 // i.e Write something to make sure ticket was saved to the database
 it('creates a ticket with valid inputs', async () => {
   // Add in a check to make sure a ticket was saved
+
+  // Get all the tickets that exists insode of that Ticket collection, every
+  // last one
+  // When this test first runs, there should be 0 collections as there's
+  // logic for that in the beforeEach hook fn in the setup for JEST
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = 'coachella';
+
   await request(app)
     .post('/api/tickets')
+    .set('Cookie', global.signin())
     .send({
-      title: 'coachella',
+      // title: 'coachella',
+    title,
       price: 20,
     })
     .expect(201);
+
+  // After making the request, now we expect our collection to have exactly
+  // 1 ticket inside there
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  // Look at the record that was actually created and write out an assertion
+  // abuot its title and its price
+  expect(tickets[0].price).toEqual(20);
+  expect(tickets[0].title).toEqual(title);
 });
--- tickets/src/routes/new-ticket.ts
+++ tickets/src/routes/new-ticket.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@tixvibe/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -14,8 +15,33 @@ router.post(
       .withMessage('Price must be greater than 0'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    // res.sendStatus(200);
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      // We have access to currentUser on the req object, because currentUser
+      // middleware is executed back inside of the app.ts. That's what sets the
+      // req.currentUser property
+      // We have to make sure currentUser actually exists
+      // We don't really have to do that because requireAuth middleware above,
+      // already does that for us. Inside there is a check to see if currentUser
+      // is defined, if not it throws NotAuthorizedError and we never continue on
+      // to the next middleware or possibly the next request handler in this case
+      // So, right now we're never going to end up in this statement right here
+      // if currentUser is undefined. TypeScript just doesn't understand that
+      // because it is not looking into the requireAuth middleware fn and understanding
+      // that we're going to return early out of this entire chain if currentUser
+      // isn't defined
+      // So we're very confident about the `!` and tell TS, don't sweat it,
+      // currentUser is defined!
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
   },
 );
 
