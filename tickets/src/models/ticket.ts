4f25f443b99d2fc679be7c6d7e5087faef5249fc
Colson <colsoncalifornication@gmail.com>
2024-08-30 12:20:12 +0545

    create Ticket schema and the actual model

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -20,3 +20,42 @@ interface TicketDoc extends mongoose.Document {
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // Massage that id property
+    toJSON: {
+      transform(doc, ret) {
+        // ret is gonna be the object thats just about to turned into JSON
+        // So we're going to make a direct changes to that ret object
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+// This is going to be the one and only way that we create new records!
+// Just to make sure we can have TS helping us figure out the different
+// types of attributes we're supposed to providing.
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket };
