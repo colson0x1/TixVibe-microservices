b7254e568b276fb193273a0d41179fec270e7de0
Colson <colsoncalifornication@gmail.com>
2024-09-07 14:58:08 +0545

    export enum subject - leverage TypeScript for Listener validation: make sure right properties are being accessed on the event data object, right name is used for the events or the subject name so that theres a valid subject and valid type of data flowing into onMessage i.e make TypeScript to somehow validate the pairing of the subject and type of the data argument to onMessage  :: make sure TypeScript can make sure whenever listener is created, it will give us some matching subject and data type argument inside the onMessage fn

---
+++ nats-streaming-server/src/events/subjects.ts
@@ -0,0 +1,6 @@
+// Subjects in the world of NATS is the name of the Channel
+// We can think of it Kind of like an event name
+export enum Subjects {
+  TicketCreated = 'ticket:created',
+  OrderUpdated = 'order:updated',
+}
