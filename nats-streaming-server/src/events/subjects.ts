a3ba8837e5bac1f762ad7258685a3aad4fbc66bb
Colson <colsoncalifornication@gmail.com>
2024-09-07 15:27:12 +0545

    custom event interface - come up with a structure thats going to allow TS to understand there is a type coupling of sorts between a ticket subject and event data type of id, title and price :: use interface to describe the very distinct coupling between particular subject and event data expected to receive with it

--- nats-streaming-server/src/events/subjects.ts
+++ nats-streaming-server/src/events/subjects.ts
@@ -1,5 +1,7 @@
+// @ Listing of all the possible Subjects
 // Subjects in the world of NATS is the name of the Channel
 // We can think of it Kind of like an event name
+
 export enum Subjects {
   TicketCreated = 'ticket:created',
   OrderUpdated = 'order:updated',
