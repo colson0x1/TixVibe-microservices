fa2c15a68589ba846f3b9da4fa2a5a365a78d938
Colson <colsoncalifornication@gmail.com>
2024-09-23 14:43:49 +0545

    provide a mock implementation:  provide fake implementation of just the parts that are expected to be implemented by those other files inside nats-wrapper: new-ticket route handler will try to import NatsWrapper file

--- tickets/src/__mocks__/nats-wrapper.ts
+++ tickets/src/__mocks__/nats-wrapper.ts
@@ -1,2 +1,11 @@
-/* Fake implementation for Nats Wrapper for TEST Environment */
-export const natsWrapper = {};
+// Fake implementation for Nats Wrapper for TEST Environment
+/* @ Nats Wrapper instance/object
+ * { _client: Stan, client: Stan, connect: () => Promise }
+ */
+export const natsWrapper = {
+  client: {
+    publish: (subject: string, data: string, callback: () => void) => {
+      callback();
+    },
+  },
+};
