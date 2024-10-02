cd29fad2e8287850995a8aba03b1a12fff0cf7b0
Colson <colsoncalifornication@gmail.com>
2024-10-02 09:34:24 +0545

    subtle service coupling - create order route handler: make sure user can create a post request to create an order + make sure only people who are authenticated are able to access this route handler i.e make sure the user is authenticated and they are passing some ticket id they are trying to purchase on the body of the request + make sure whatever ticketId user provided has the structure of a real Mongo ID

--- orders/src/routes/new-order.ts
+++ orders/src/routes/new-order.ts
@@ -1,9 +1,28 @@
+import mongoose from 'mongoose';
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@tixvibe/common';
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // Subtle service coupling
+      // Make sure user is providing a valid Mongo ID
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  // If anything goes wrong with the validation step, we reject the request
+  // right away and send back an error message with validateRequest middleware
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  },
+);
 
 export { router as newOrderRouter };
