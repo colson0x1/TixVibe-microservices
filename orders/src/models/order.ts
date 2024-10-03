d47bdfe4a96be0c9800ad1779558f8a71aed3ce2
Colson <colsoncalifornication@gmail.com>
2024-10-03 10:46:41 +0545

    create Order model -> associate orders and tickets: take in some information about a user that is trying to purchase a ticket and create an order record and save the order to the MongoDB -> create an Mongoose Model to describe what an Order is: make Order collection somehow reference a ticket from Ticket collection + once the user tries to create an order, mark that ticket somehow as being reserved i.e associate an order with a ticket: use ref/population features inside of Mongoose to associate Orders document with Ticket document :: write implementation for the Order document and Ticket document + use enums to define the status of a order: make all services have an exact definition of what different status an order can have

---
+++ orders/src/models/order.ts
@@ -0,0 +1,70 @@
+import mongoose from 'mongoose';
+
+// Properties that are required to create an Order
+interface OrderAttrs {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  // ticket is an instance of TicketDoc
+  ticket: TicketDoc;
+}
+
+// Properties that actually end up on an Order
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: string;
+  expiresAt: Date;
+  // ticket is an instance of TicketDoc
+  ticket: TicketDoc;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  // All we are saying is, this Model (i.e Collection) has one extra little
+  // method called `build()` for type checking when create an Order Document
+  // Its gonna take an argument called `attrs` that is of type `OrderAttrs`
+  // and out of that, we'll get an `OrderDoc`
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+// Schema that actually describes the different properties and rules about them
+// that we're gonna have on an instance of an Order
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      // Make sure every order that gets created has a user tied to it
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+// Define `build` method on the Order Schema static's object which is going
+// to give us the build method on the actual Order model
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+// `model` is a generic fn
+// 'Order' is the name of the collection
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
