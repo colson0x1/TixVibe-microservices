dcffc2eb0ebb274873c18a9ec2a51accf547ee8c
Colson <colsoncalifornication@gmail.com>
2024-10-19 15:35:27 +0545

    assert reserved tickets: return an error if the ticket is already reserved: create a ticket, save it to the database, then create an order, save it to the database and make sure the order and the ticket are associated with each other and then finally make the request

--- orders/src/routes/__test__/new-order.test.ts
+++ orders/src/routes/__test__/new-order.test.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../../app';
+import { Order, OrderStatus } from '../../models/order';
+import { Ticket } from '../../models/ticket';
 
 // Test for auth
 // Test for body validation i.e provide some valid id for the ticket id in
@@ -18,6 +20,31 @@ it('returns an error if the ticket does not exist', async () => {
     .expect(404);
 });
 
-it('returns an error if the ticket is already reserved', async () => {});
+it('returns an error if the ticket is already reserved', async () => {
+  // First create a ticket and save it to the database
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 2000,
+  });
+  await ticket.save();
+
+  // Create an order and associate that above ticket with this order and put
+  // the order in the correct state as well
+  const order = Order.build({
+    // Ticket is the ticket created above
+    ticket,
+    userId: 'macbookpro',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    // Send along the id of the ticket that we're trying to reserve
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
 
 it('reserves a ticket', async () => {});
