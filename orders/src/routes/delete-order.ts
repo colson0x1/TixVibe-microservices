050c16d7960ab7a658efa628c4ebdad561b6b342
Colson <colsoncalifornication@gmail.com>
2024-10-22 11:06:24 +0545

    cancel an order -> when request is sent, change the status of the order to cancel instead of deleting it from the db: make sure the user is authenticated + make sure to find some order inside the database + make sure the user who is making this request owns this order as well

--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -1,9 +1,40 @@
 import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  NotAuthorizedError,
+  NotFoundError,
+} from '@tixvibe/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
-});
+// We are not really deleting a request, we're canceling it
+// It's better to use `patch` instead of `delete` here!!
+router.delete(
+  '/api/orders/:orderId',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    // if owner of the order is not equal to user who is making the request, then
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    // Update our order status
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    // res.send(order);
+    // If we're really deleting this request, we need to also pass .status(204)
+    res.status(204).send(order);
+  },
+);
 
 export { router as deleteOrderRouter };
