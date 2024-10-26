879d138914c4412ce8f1e25613e312de835cf9fa
Colson <colsoncalifornication@gmail.com>
2024-10-26 14:32:36 +0545

    onMessage implementation - get information about a ticket from Tickets Service emitting ticket:created event and also save the ticket information on a local tickets collection inside of Orders Service i.e data replication between different services -> saving information about a ticket so that when the Orders Service needs to know some details about a ticket, it does not have to do some synchronous communication over to the Tickets Service to learn about the different tickets that are available

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -21,5 +21,26 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   // i.e `TicketCreatedEvent` and we're saying, off that interface, take a
   // look at the `data` property and thats gonna be the type of this `data`
   // argument on `onMessage`
-  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
+  // Message is the object in node-nats-streaming that has many methods
+  // out of which `ack()` being the most important method
+  async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    // Take a loook at the `data` from the argument
+    // The only property we really want to persist inside of Orders Service is
+    // the `title` and `price`
+    // Its already defined inside of Ticket Model `models/ticket.ts` interface
+    // inside of Orders Service
+    // So pull off title and price from data
+    const { title, price } = data;
+    // Use that to build a new ticket
+    const ticket = Ticket.build({
+      title,
+      price,
+    });
+    // And then save it
+    await ticket.save();
+
+    // Ack that message to tell NATS streaming server that we've successfully
+    // processed this thing and we're good to go!
+    msg.ack();
+  }
 }
