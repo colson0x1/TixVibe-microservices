c27965f46f048ed8b630291721d8fcd130c26b9d
Colson <colsoncalifornication@gmail.com>
2024-11-19 23:46:13 +0545

    configure Payments Service: add all the base configurations + install dependencies + build a docker image / push it to docker hub + configure skaffold.yaml to handle code sync for Payment Service + create a kubernetes deployment for payments deployment and also a configure mongodb instance for Payments Service

---
+++ payments/src/app.ts
@@ -0,0 +1,43 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@tixvibe/common';
+
+const app = express();
+// Traffic has been proxied to our application through Ingress NGINX. Express
+// is going to see the fact that stuff is being proxied and by default. Express
+// by default doesn't really trust this HTTPS connection
+// @ Make Express aware that its behind a Proxy of Ingress NGINX and to make sure
+// it should still trust traffic as being secure even though its coming form
+// that proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    // Disables encryption
+    signed: false,
+    // Require that cookies will only be used if the user is visiting the
+    // application over an HTTPS connection
+    // secure: true,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+// Making sure we add this middleware after cookie-session. Because
+// cookie-session has to run first so it can take a look at the cookie and
+// set the `req.session` property else if we don't do that, whenever currentUser
+// runs, it will be running to soon and req.session will not be set
+// So now, if the user is authenticated, that will set the req.currentUser property
+app.use(currentUser);
+
+/* app.all('*', async (req, res, next) => {
+  next(new NotFoundError());
+}); */
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/index.ts
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  // Detect error immediately and throw an error when we start to deploy
+  // our code
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID must be defined');
+  }
+
+  // Connect to MongoDB instance successfully through Mongoose before we ever
+  // even startup our Express application or technically start listenign to
+  // traffic at port 3000
+  // Just as this application really needs to have a connection to MongoDB to
+  // work successfully, it also needs to have a connection to NATS Streaming
+  // Server in order for it to work successfully!
+  try {
+    // await natsWrapper.connect('tixvibe', 'stillhome', 'htttp://nats-srv:4222');
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+    // Exit process entirely anytime we lose connection to NATS Streaming Server
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    // @ Graceful shutdown anytime a Client is about to close down
+    // Handlers to watch for any single time that someone tries to close down
+    // this process
+    // These event handlers are watching for interrupt signals or terminate signals
+    // @ SIGINT - Signal Intercept (Ex: rs while running ts-node-dev)
+    // @ SIGTERM - Signal Terminate (Ex: killing terminal with Ctrl+C)
+    // i.e Close the client manually as well anytime we get interrupt signal
+    // or terminate singal
+    // `this._client!` or alternatively `this.client` which is better way
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    // Right after NATS connection stuff, create an instnace of both of those
+    // listeners and pass in natsWrapper client
+    // NOTE: Just creating an instance is not enough. Its the listen method/function
+    // that actually tells the listener to start listening for incoming events
+
+    /* this._client.on('connect', () => {
+      console.log('Connected to NATS')
+    }) */
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  // If DB connects successfully above, then its actually time for listening
+  // traffic
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!');
+  });
+};
+
+start();
