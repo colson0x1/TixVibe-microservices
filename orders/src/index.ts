263074ef5e1950b1b69c810dcaa706576711c0c1
Colson <colsoncalifornication@gmail.com>
2024-11-19 17:40:56 +0545

    listen for expiration: create an instance of expiration listener in index.ts inside Orders Service and tell it to start listening for incoming events

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -3,6 +3,7 @@ import { app } from './app';
 import { natsWrapper } from './nats-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 const start = async () => {
   // Detect error immediately and throw an error when we start to deploy
@@ -62,6 +63,7 @@ const start = async () => {
     // after above is a good time to start listening for incoming events
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
