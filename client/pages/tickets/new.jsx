50a8badbf64a96908bed7143e984300a3f2024ea
COLSON <colsoncalifornication@gmail.com>
2024-12-06 01:50:49 +0545

    ticket creation - make a request to Tickets Service to create a new ticket: make POST request to /api/tickets and include title and price on the body + use use-request helper hook to make a request from inside of a component itself

--- client/pages/tickets/new.jsx
+++ client/pages/tickets/new.jsx
@@ -1,8 +1,28 @@
 import { useState } from 'react';
+import useRequest from '../../hooks/use-request';
 
 const NewTicket = () => {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/tickets',
+    // Method is lowercase and not uppercase because we're really just taking
+    // this method right here and looking up the appropriate method on the
+    // axios library object i.e use-request.js -> `axios[method](url, body)`
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    // onSuccess returns the response data which will really be a ticket
+    onSuccess: (ticket) => console.log(ticket),
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
 
   const onBlur = () => {
     const value = parseFloat(price);
@@ -17,7 +37,7 @@ const NewTicket = () => {
   return (
     <div>
       <h1 style={{ color: 'dodgerblue' }}>Create a Ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label>Title</label>
           <input
@@ -35,6 +55,7 @@ const NewTicket = () => {
             className='form-control'
           />
         </div>
+        {errors}
         <button className='btn btn-primary'>Submit</button>
       </form>
     </div>
