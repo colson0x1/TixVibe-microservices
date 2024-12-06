090999938dcfa7ff9f074c8ae7fefe075e627364
COLSON <colsoncalifornication@gmail.com>
2024-12-06 17:24:59 +0545

    create an order - implement getInitialProps function to load up some information about the ticket thats being displayed to the user + display ticket information + present a button to the user that can be used to purchase the displayed ticket i.e create an order because that is how a user expresses the intent to purchase a ticket inside the app -> show an purchase button and whenever clicks that, make a request to the backend specifically the Orders Service an attempt to create an order referencing this ticket

--- client/pages/tickets/[ticketId].jsx
+++ client/pages/tickets/[ticketId].jsx
@@ -1,5 +1,40 @@
-const TicketShow = () => {
-  return <div>TicketShow</div>;
+import useRequest from '../../hooks/use-request';
+
+const TicketShow = ({ ticket }) => {
+  const { doRequest, errors } = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: {
+      // body is going to be the id of the ticket that we want to purchase
+      ticketId: ticket.id,
+    },
+    // onSuccess is going to be invoked with whatever response we get back
+    // from the request
+    // So in this case, hopefully it will be the `order` that was created
+    onSuccess: (order) => console.log(order),
+  });
+
+  return (
+    <div>
+      <h1>{ticket.title}</h1>
+      <h4>Price: {ticket.price}</h4>
+      {errors}
+      <button onClick={doRequest} className='btn btn-primary'>
+        Purchase
+      </button>
+    </div>
+  );
+};
+
+TicketShow.getInitialProps = async (context, client) => {
+  // We're specifically pulling out a property called ticketId because that
+  // is what I named this file i.e `[ticketId]`
+  const { ticketId } = context.query;
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+  // This returned object right here is going to get merged into all the
+  // different props that are provided to TicketShow component
+  return { ticket: data };
 };
 
 export default TicketShow;
