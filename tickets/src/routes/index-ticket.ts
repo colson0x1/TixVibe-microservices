e9a3c128ddc90e72ef9bc3364477302601249601
COLSON <colsoncalifornication@gmail.com>
2024-12-07 00:23:08 +0545

    filter reserved tickets - only show tickets that are available for purchase ::: note -> it is the presence of the orderId property that indicates that a ticket is currently reserved and cannot be ordered :: add a little filter/criteria on the query to get list of tickets i.e only give tickets where the orderId property is undefined

--- tickets/src/routes/index-ticket.ts
+++ tickets/src/routes/index-ticket.ts
@@ -8,7 +8,14 @@ router.get('/api/tickets', async (req: Request, res: Response) => {
   // any kind of conditions on what records we actually find.
   // We're putting in an empty object here to say, just give us all the
   // tickets inside this collection
-  const tickets = await Ticket.find({});
+  // const tickets = await Ticket.find({});
+  // NOTE: It is the presence of the orderId property that indicates that a
+  // ticket is currently reserved and cannot be ordered.
+  // Only get tickets whose orderId property is not defined
+  const tickets = await Ticket.find({
+    // Find all of the tickets that don't have corresponding order
+    orderId: undefined,
+  });
 
   res.send(tickets);
 });
