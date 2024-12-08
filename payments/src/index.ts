9924b95d40e443d1939486579c2e26e044037808
COLSON <colsoncalifornication@gmail.com>
2024-12-08 12:33:07 +0545

    test 2: service specific test - Payments Service

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -5,6 +5,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
+  console.log('Starting...');
   // Detect error immediately and throw an error when we start to deploy
   // our code
   if (!process.env.JWT_KEY) {
